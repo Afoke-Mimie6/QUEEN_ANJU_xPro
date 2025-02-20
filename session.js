5c046c1ed0059daf6c7a0dd3d69ecd4f49028a1c
₲σԃмσтнєя Ɣ̟̟₋₵ƐƐ💍⃟༑⌁ <112706742+Afoke-Mimie6@users.noreply.github.com>
2025-02-20 15:42:33 +0100

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~3Y9XwLpJ#lznUkTQDzCKeOE_vs4Luw_Hkk8S6xIleDU2_Jp9eQvY",
 PORT: process.env.PORT || "8000"
 };
